386b87b9851b1190eb7a8b0db43274da7d8e960e
Rheeda-boo <rheeda.beecha@gmail.com>
2020-10-19 13:06:32 +0000

    Mute and Unmute features enabled

--- public/script.js
+++ public/script.js
@@ -14,7 +14,7 @@ let myVideoStream
 
 navigator.mediaDevices.getUserMedia({
     video: true,
-    audio: false
+    audio: true
 }).then(stream => {
     myVideoStream = stream;
     addVideoStream(myVideo, stream);
@@ -73,3 +73,33 @@ const scrollToBotton = () => {
     let d = $(".chatWindow");
     d.scrollTop(d.prop("scrollHeight"));
 }
+
+// Mute audio
+const muteUnmute = () => {
+    const enabled = myVideoStream.getAudioTracks()[0].enabled;
+    if (enabled) {
+        myVideoStream.getAudioTracks()[0].enabled = false;
+        setUnmuteButton();
+    }
+
+    else {
+        setMuteButton();
+        myVideoStream.getAudioTracks()[0].enabled = true;
+    }
+}
+
+const setMuteButton = () => {
+    const html = `
+      <i class="fas fa-microphone"></i>
+      <span>Mute</span>
+    `
+    document.querySelector('.main__mute_button').innerHTML = html;
+  }
+  
+  const setUnmuteButton = () => {
+    const html = `
+      <i class="unmute fas fa-microphone-slash"></i>
+      <span>Unmute</span>
+    `
+    document.querySelector('.main__mute_button').innerHTML = html;
+  }
